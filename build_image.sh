fdaf7f7d4be30127efad06e1beb58cb527b2f3f0
Aksel <akselsloan@gmail.com>
2024-11-20 23:49:21 +0000

    Added docker related files.

---
+++ build_image.sh
@@ -0,0 +1,5 @@
+# build the mouss_mini docker image
+# this file includes an options that should be 
+# used in a normal build. 
+
+docker build -t mouss_mini_img -f docker/Dockerfile .
---
+++ docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#####################################################################
+#####################################################################
+##  File:   entrypoint.sh
+##
+##  Spec:   This script sets handle's the sourcing for the 
+##          mouss_mini ros2 deployment. This script runs on
+##          startup in parallel to startup.h    
+##
+##  notes:  Changes here on only reflect after a fresh docker build! 
+##
+##  Author: Aksel Sloan | aksel.sloan@noaa.gov
+##
+##
+#####################################################################
+#####################################################################
+
